3373c53e475c6f2726139c3c36de41da77f2be8f
Mayank <mayank.raj@accoliteindia.com>
2019-01-22 20:42:16 +0530

    maven assignment

---
+++ Maven/Course.java
@@ -0,0 +1,60 @@
+package hibernetAssignment.mayank;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Course")
+public class Course {
+
+	@Id
+	@Column(name="courseId")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Integer courseId;
+
+	@Column(name="courseName")
+	public String courseName;
+
+	@Column(name="studentId")
+	public Integer studentId;
+
+
+	
+	public Integer getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+
+}
